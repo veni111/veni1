4a0a8f590276c99b201c8303e3fe2ab231f02022
veni111 <62224402+veni111@users.noreply.github.com>
2021-03-24 10:38:27 +0000

    Add files via upload

---
+++ apex_applications/sateon/comments.sql
@@ -0,0 +1,17 @@
+prompt --application/comments
+begin
+--   Manifest
+--     APPLICATION COMMENTS: 270
+--   Manifest End
+wwv_flow_api.component_begin (
+ p_version_yyyy_mm_dd=>'2020.10.01'
+,p_release=>'20.2.0.00.20'
+,p_default_workspace_id=>1501503556929603
+,p_default_application_id=>270
+,p_default_id_offset=>11548359986158893
+,p_default_owner=>'MGR_APEX'
+);
+null;
+wwv_flow_api.component_end;
+end;
+/
